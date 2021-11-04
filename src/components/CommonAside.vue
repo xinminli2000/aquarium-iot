b5a812dc24d47011247dc02083067348a1366277
xinminli2000 <xinminli2000@gmail.com>
2021-11-04 16:11:37 +0800

    左侧导航栏组件初步完成，使用固定数据

--- src/components/CommonAside.vue
+++ src/components/CommonAside.vue
@@ -1,9 +1,75 @@
 <template>
-  <div>aside</div>
+  <el-menu default-active="2" class="el-menu-vertical-demo" background-color="#33aef0" text-color="#fff" active-text-color="#ffd04b">
+    <el-menu-item :index="item.path" v-for="item in noChildren" :key="item.path">
+      <i :class="'el-icon-' + item.icon"></i>
+      <span slot="title">{{ item.label }}</span>
+    </el-menu-item>
+    <el-submenu index="item.path" v-for="(item, index) in hasChildren" :key="index">
+      <template slot="title">
+        <i :class="'el-icon-' + item.icon"></i>
+        <span>{{ item.label }}</span>
+      </template>
+      <el-menu-item-group>
+        <el-menu-item index="subItem.path" v-for="(subItem, subIndex) in item.children" :key="subIndex"
+          ><i :class="'el-icon-' + subItem.icon"></i>{{ subItem.label }}</el-menu-item
+        >
+      </el-menu-item-group>
+    </el-submenu>
+  </el-menu>
 </template>
 
 <script>
-export default {}
+export default {
+  computed: {
+    noChildren() {
+      return this.asideMenu.filter((item) => !item.children)
+    },
+    hasChildren() {
+      return this.asideMenu.filter((item) => item.children)
+    }
+  },
+  data() {
+    return {
+      asideMenu: [
+        {
+          path: '/',
+          label: '首页',
+          icon: 'home'
+        },
+        {
+          path: '/user',
+          label: '用户管理',
+          icon: 'user'
+        },
+        {
+          path: '/tank',
+          label: '鱼缸管理',
+          icon: 'box'
+        },
+        {
+          label: '其它管理',
+          icon: 'copy-document',
+          children: [
+            {
+              path: 'other1',
+              label: '其它1',
+              icon: 'trophy'
+            },
+            {
+              path: 'other2',
+              label: '其它2',
+              icon: 'trophy-1'
+            }
+          ]
+        }
+      ]
+    }
+  }
+}
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.el-menu {
+  height: 100%;
+}
+</style>
